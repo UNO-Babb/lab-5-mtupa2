999bbd6f244d344d7b39bd7e12b6239365165a97
mtupa2 <17500380@nebraska.edu>
2025-09-29 00:52:20 +0000

    done with lab

--- WordGame.py
+++ WordGame.py
@@ -3,38 +3,54 @@
 import random
 
 def inWord(letter, word):
-    """Returns boolean if letter is anywhere in the given word"""
-
-    return False
+    """Returns True if letter is anywhere in the given word"""
+    return letter in word
 
 def inSpot(letter, word, spot):
-    """Returns boolean response if letter is in the given spot in the word."""
-
-    return False
+    """Returns True if letter is in the given spot in the word"""
+    return word[spot] == letter
 
 def rateGuess(myGuess, word):
-    """Rates your guess and returns a word with the following features.
+    """Rates your guess and returns feedback:
     - Capital letter if the letter is in the right spot
-    - Lower case letter if the letter is in the word but in the wrong spot
-    - * if the letter is not in the word at all"""
-
+    - Lowercase letter if the letter is in the word but wrong spot
+    - * if the letter is not in the word at all
+    """
+    result = ""
+    for i in range(len(myGuess)):
+        letter = myGuess[i]
+        if inSpot(letter, word, i):
+            result += letter.upper()
+        elif inWord(letter, word):
+            result += letter.lower()
+        else:
+            result += "*"
+    return result
 
 def main():
-    #Pick a random word from the list of all words
-    wordFile = open("words.txt", 'r')
-    content = wordFile.read()
-    wordList = content.split("\n")
-    todayWord = random.choice(wordList)
-    print(todayWord)
+    # Load word list and pick a random word
+    with open("words.txt", 'r') as wordFile:
+        wordList = wordFile.read().splitlines()
+    todayWord = random.choice(wordList).upper()
 
-    #User should get 6 guesses to guess
+    print("Welcome to the Word Guessing Game!")
+    print(f"Guess the {len(todayWord)}-letter word. You have 6 tries.")
 
-    #Ask user for their guess
-    #Give feedback using on their word:
+    for attempt in range(6):
+        guess = input(f"Guess {attempt + 1}: ").upper()
 
+        if len(guess) != len(todayWord):
+            print(f"Please enter a {len(todayWord)}-letter word.")
+            continue
 
+        feedback = rateGuess(guess, todayWord)
+        print("Feedback:", feedback)
 
-
+        if guess == todayWord:
+            print("You guessed the word correctly!")
+            break
+    else:
+        print(f"Out of guesses! The word was: {todayWord}")
 
 if __name__ == '__main__':
-  main()
+    main()
